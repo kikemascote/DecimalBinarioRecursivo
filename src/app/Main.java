a52c028f33ed09e752ca0903b4737496a8690b35
XAVIER RAYAS <xavirayas@gmail.com>
2017-10-07 14:55:55 -0600

    Creacion del metodo para convertir a binario

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,21 @@
 package app;
 
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		
+		Scanner keyBoard = new Scanner(new InputStreamReader(System.in));
+		int n;
+		System.out.print("Escribe el numero decimal que desea convertir a binario: ");
+		n=keyBoard.nextInt();
+		if (n>0) {
+			System.out.println(Convert.toBinary(n));
+		}else{
+			System.out.println("000");
+		}
 
 	}
 
